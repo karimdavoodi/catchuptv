ac4feb53a224e14739b7528150a8adcbedb142f4
Karim Davoodi <asehabi2020@gmail.com>
2021-04-04 15:01:18 +0300

    init....

---
+++ src/cpp-lib/CMakeFiles/testlib.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/opt/catchuptv/src/cpp-lib/src/main.cpp" "/opt/catchuptv/src/cpp-lib/CMakeFiles/testlib.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_ATOMIC_DYN_LINK"
+  "BOOST_CHRONO_DYN_LINK"
+  "BOOST_DATE_TIME_DYN_LINK"
+  "BOOST_FILESYSTEM_DYN_LINK"
+  "BOOST_LOG_DYN_LINK"
+  "BOOST_LOG_DYN_LINK=1"
+  "BOOST_LOG_SETUP_DYN_LINK"
+  "BOOST_REGEX_DYN_LINK"
+  "BOOST_SYSTEM_DYN_LINK"
+  "BOOST_THREAD_DYN_LINK"
+  "GST_USE_UNSTABLE_API=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/gstreamer-1.0"
+  "/usr/include/glib-2.0"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/opt/catchuptv/src/cpp-lib/CMakeFiles/utils.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/cpp-lib/CMakeFiles/utils.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/utils.dir/src/gst.cpp.o"
+  "CMakeFiles/utils.dir/src/mq.cpp.o"
+  "CMakeFiles/utils.dir/src/utils.cpp.o"
+  "libutils.a"
+  "libutils.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/utils.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
