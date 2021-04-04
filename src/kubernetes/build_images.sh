4294be0d6f9f96d68be883b080b6038f71cca97b
Your Name <you@example.com>
2021-04-04 18:35:48 +0300

    change...

---
+++ src/kubernetes/build_images.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CUR=`pwd`
+for dir in chan-net-to-epg gb-mq; do
+    cd ../$dir
+    echo "###################################################  Build $dir"
+    docker build .  --network host -t localhost:32000/$dir:registry
+
+    echo "Push $dir"
+    docker push localhost:32000/$dir
+done
+cd $CUR
+
+
